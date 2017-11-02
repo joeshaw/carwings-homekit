8993d431c32412fae6a5bd65c9d882ac0a669f5c
Joe Shaw <joe@joeshaw.org>
2017-11-02 00:27:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# carwings-homecontrol
+
+HomeKit support for the Nissan Leaf using
+[HomeControl](https://github.com/brutella/hc) and my [Carwings Go
+library](https://github.com/joeshaw/carwings).
+
+When running, this service publishes a single HomeKit accessory
+exposing three services:
+
+1. A battery service indicating the current charge of your Leaf and
+   its charging status.
+1. A switch service indicating whether the Leaf is currently charging.
+   If the Leaf is plugged in but not charging, you can flip this
+   switch on to begin charging the vehicle.
+1. A fan (v2) service for the Leaf's climate control.  Flipping this
+   switch toggles the vehicle's climate control system.
+
+After the vehicle is paired with your iOS Home app, you can control it
+with any service that integrates with HomeKit, including Siri ("How
+much battery does the Leaf have?") and Apple Watch.  If you have a
+home hub like an Apple TV or iPad, you can control the Leaf remotely.
+
+## Installing
+
+The tool can be installed with:
+
+    go get -u github.com/joeshaw/carwings-homecontrol
+
+You will need to create a `config.json` file with your Carwings
+username and password, like so:
+
+```json
+{
+    "username": "foo@example.com",
+    "password": "carwingsPassw0rd"
+}
+```
+
+Then you can run the service:
+
+    carwings-homecontrol -config config.json
+
+The service will make an initial call to the Carwings service to get
+updated and current battery and climate control information, and then
+expose the service.
+
+To pair, open up your Home iOS app, click the + icon, choose "Add
+Accessory" and then tap "Don't have a Code or Can't Scan?"  You should
+see the Leaf under "Nearby Accessories."  Tap that and enter the PIN
+00102003 (or whatever you chose in `config.json`).  You should see two
+entries appear in your list, one for the charging switch and one for
+the climate control fan.
+
+## Contributing
+
+Issues and pull requests are welcome.  When filing a PR, please make
+sure the code has been run through `gofmt`.
+
+## License
+
+Copyright 2017 Joe Shaw
+
+`carwings-homecontrol` is licensed under the MIT License.  See the LICENSE file
+for details.
+
+
---
+++ main.go
@@ -0,0 +1,307 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/brutella/hc"
+	"github.com/brutella/hc/accessory"
+	"github.com/brutella/hc/characteristic"
+	"github.com/brutella/hc/service"
+	"github.com/joeshaw/carwings"
+)
+
+type Leaf struct {
+	sess *carwings.Session
+
+	battUpdate chan chan struct{}
+	hvacUpdate chan chan struct{}
+
+	acc       *accessory.Accessory
+	battSvc   *service.BatteryService
+	hvacSvc   *service.FanV2
+	chargeSvc *service.Switch
+}
+
+type Config struct {
+	// Storage path for information about the HomeKit accessory.
+	// Defaults to ~/.homecontrol
+	StoragePath string `json:"storage_path"`
+
+	// Carwings username (email address)
+	Username string `json:"username"`
+
+	// Carwings password
+	Password string `json:"password"`
+
+	// Carwings region.  Defaults to "NNA" (United States)
+	Region string `json:"region"`
+
+	// Accessory name.  Defaults to "Leaf"
+	AccessoryName string `json:"accessory_name"`
+
+	// HomeKit PIN.  Defaults to 00102003.
+	HomekitPIN string `json:"homekit_pin"`
+}
+
+func main() {
+	var configFile string
+
+	flag.StringVar(&configFile, "config", "config.json", "config file")
+	flag.Parse()
+
+	// Default values
+	config := Config{
+		StoragePath:   filepath.Join(os.Getenv("HOME"), ".homecontrol"),
+		Region:        "NNA",
+		AccessoryName: "Leaf",
+		HomekitPIN:    "00102003",
+	}
+
+	f, err := os.Open(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&config); err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log.Println("Connecting to Carwings service")
+	s, err := carwings.Connect(config.Username, config.Password, config.Region)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to Carwings")
+
+	info := accessory.Info{
+		Name:         config.AccessoryName,
+		Manufacturer: "Nissan",
+		Model:        "Leaf",
+		SerialNumber: s.VIN,
+	}
+
+	leaf := &Leaf{
+		sess:       s,
+		battUpdate: make(chan chan struct{}),
+		hvacUpdate: make(chan chan struct{}),
+		acc:        accessory.New(info, accessory.TypeOther),
+		battSvc:    service.NewBatteryService(),
+		chargeSvc:  service.NewSwitch(),
+		hvacSvc:    service.NewFanV2(),
+	}
+
+	leaf.acc.AddService(leaf.battSvc.Service)
+	leaf.acc.AddService(leaf.hvacSvc.Service)
+	leaf.acc.AddService(leaf.chargeSvc.Service)
+
+	leaf.chargeSvc.On.OnValueRemoteUpdate(func(on bool) {
+		if !on {
+			log.Println("Charging cannot be switched off")
+			leaf.chargeSvc.On.SetValue(true)
+			return
+		}
+
+		log.Println("Sending charging request")
+		if err := leaf.sess.ChargingRequest(); err != nil {
+			log.Printf("Error requesting charging: %v", err)
+			return
+		}
+
+		// Request a battery status update
+		leaf.battUpdate <- make(chan struct{})
+	})
+
+	leaf.hvacSvc.Active.OnValueRemoteUpdate(func(active int) {
+		switch active {
+		case characteristic.ActiveInactive:
+			log.Println("Sending request to turn off climate control")
+			key, err := leaf.sess.ClimateOffRequest()
+			if err != nil {
+				log.Printf("Error requesting climate off: %v", err)
+				return
+			}
+
+			if err := waitOnKey(ctx, key, leaf.sess.CheckClimateOffRequest); err != nil {
+				log.Printf("Error requesting climate off (%s): %v", key, err)
+				return
+			}
+
+		case characteristic.ActiveActive:
+			log.Println("Sending request to turn on climate control")
+			key, err := leaf.sess.ClimateOnRequest()
+			if err != nil {
+				log.Printf("Error requesting climate on: %v", err)
+				return
+			}
+
+			if err := waitOnKey(ctx, key, leaf.sess.CheckClimateOnRequest); err != nil {
+				log.Printf("Error requesting climate on (%s): %v", key, err)
+				return
+			}
+		}
+
+		// Request a climate status update
+		leaf.hvacUpdate <- make(chan struct{})
+	})
+
+	hcConfig := hc.Config{
+		Pin:         config.HomekitPIN,
+		StoragePath: filepath.Join(config.StoragePath, info.Name),
+	}
+	t, err := hc.NewIPTransport(hcConfig, leaf.acc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hc.OnTermination(func() {
+		cancel()
+		<-t.Stop()
+	})
+
+	go updateBattery(ctx, leaf)
+	go updateClimate(ctx, leaf)
+
+	// Update battery and climate status every 15m
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(15 * time.Minute):
+				leaf.battUpdate <- make(chan struct{})
+				leaf.hvacUpdate <- make(chan struct{})
+			}
+		}
+	}()
+
+	// And update them initially, and wait for them to finish
+	// before exposing the accessory to HomeKit.
+	ch1, ch2 := make(chan struct{}), make(chan struct{})
+	leaf.battUpdate <- ch1
+	leaf.hvacUpdate <- ch2
+	<-ch1
+	<-ch2
+
+	log.Println("Starting transport...")
+	t.Start()
+}
+
+func updateBattery(ctx context.Context, leaf *Leaf) {
+	log.Println("Entering battery update loop")
+	defer log.Println("Exited battery update loop")
+
+	for {
+		var ch chan struct{}
+
+		select {
+		case <-ctx.Done():
+			return
+		case ch = <-leaf.battUpdate:
+		}
+
+		log.Println("Updating battery information")
+
+		key, err := leaf.sess.UpdateStatus()
+		if err != nil {
+			log.Printf("Error getting battery status: %v", err)
+			close(ch)
+			continue
+		}
+
+		if err := waitOnKey(ctx, key, leaf.sess.CheckUpdate); err != nil {
+			log.Printf("Error updating battery status (%s): %v", key, err)
+			close(ch)
+			continue
+		}
+
+		bs, err := leaf.sess.BatteryStatus()
+		if err != nil {
+			log.Printf("Error getting battery status: %v", err)
+			close(ch)
+			continue
+		}
+
+		leaf.battSvc.BatteryLevel.SetValue(bs.StateOfCharge)
+
+		// Ideally we'd only set this if the Leaf's low
+		// battery warning was set, but the Carwings API
+		// doesn't give that to us.  So let's just say 20%.
+		lowBatt := characteristic.StatusLowBatteryBatteryLevelNormal
+		if bs.StateOfCharge <= 20 {
+			lowBatt = characteristic.StatusLowBatteryBatteryLevelLow
+		}
+		leaf.battSvc.StatusLowBattery.SetValue(lowBatt)
+
+		status := characteristic.ChargingStateNotCharging
+		if bs.ChargingStatus == carwings.NormalCharging || bs.ChargingStatus == carwings.RapidlyCharging {
+			status = characteristic.ChargingStateCharging
+		}
+		leaf.battSvc.ChargingState.SetValue(status)
+		leaf.chargeSvc.On.SetValue(status == characteristic.ChargingStateCharging)
+
+		log.Printf("Battery info update complete: %d%%, %s", bs.StateOfCharge, bs.ChargingStatus)
+		close(ch)
+	}
+}
+
+func updateClimate(ctx context.Context, leaf *Leaf) {
+	log.Println("Entering climate control update loop")
+	defer log.Println("Exited climate control update loop")
+
+	for {
+		var ch chan struct{}
+		select {
+		case <-ctx.Done():
+			return
+		case ch = <-leaf.hvacUpdate:
+		}
+
+		log.Println("Updating climate control information")
+
+		cs, err := leaf.sess.ClimateControlStatus()
+		if err != nil {
+			log.Printf("Error getting climate control status: %v", err)
+			close(ch)
+			continue
+		}
+
+		active := characteristic.ActiveInactive
+		if cs.Running {
+			active = characteristic.ActiveActive
+		}
+
+		leaf.hvacSvc.Active.SetValue(active)
+
+		log.Printf("Climate control info update complete: running %t", cs.Running)
+		close(ch)
+	}
+}
+
+func waitOnKey(ctx context.Context, key string, fn func(string) (bool, error)) error {
+	interval := 0 * time.Second
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(interval):
+		}
+		interval = 5 * time.Second
+
+		if done, err := fn(key); done {
+			return nil
+		} else if err != nil {
+			return err
+		}
+	}
+}
